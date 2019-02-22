d7203878e492b4d906acb70ad02e5037fa9c24b8
Алиса Попова <a.popova9@g.nsu.ru>
2019-02-23 03:04:43 +0700

    done catalog page

--- front/js/catalog.js
+++ front/js/catalog.js
@@ -37,16 +37,38 @@ function appendCatalog(items) {
   // отрисуй велосипеды из items в блоке <div id="bikeList">
   const parent = document.getElementById("bikeList");
   const newUl = document.createElement("ul");
+  newUl.classList.add("items-list");
   parent.appendChild(newUl);
   items.forEach(function (item) {
     let bikeNode = document.createElement("li");
+    bikeNode.classList.add("bike");
+
+    let bikeImageContainer = document.createElement("div");
     let bikeImage = document.createElement("img");
     let bikeName = document.createElement("h4");
+    let bikeCost = document.createElement("div");
+    let rentButton = document.createElement("button");
+
+    bikeImageContainer.className += "bike__img-container";
+
     bikeImage.src = `../images/${item.img}`;
-    bikeImage.width = "250";
+    bikeImage.classList.add("bike__img");
+    bikeImageContainer.appendChild(bikeImage);
+
     bikeName.innerText = item.name;
-    bikeNode.appendChild(bikeImage);
+    bikeName.classList.add("bike__name");
+
+    bikeCost.classList.add("bike__cost");
+    bikeCost.innerText = `Стоимость за час - ${item.cost} ₽`;
+
+    rentButton.className += "button button_bigger button_hovered";
+    rentButton.innerText = "Арендовать";
+
+    bikeNode.appendChild(bikeImageContainer);
     bikeNode.appendChild(bikeName);
+    bikeNode.appendChild(bikeCost);
+    bikeNode.appendChild(rentButton);
+
     parent.querySelector("ul").appendChild(bikeNode);
   });
 }
